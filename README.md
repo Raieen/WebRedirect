7c52e9f48e87ee66c1cd26ef7290a2ab8989947b
Ryan Sue <Raieen@users.noreply.github.com>
2019-08-18 20:50:50 -0400

    Updated to support multiple ports & paths

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
 # Web Redirect
 
-A small web redirect server with a simple config file that redirects a path to anything.
+A small web redirect server with a simple config that redirects paths to anything. A very easy way to redirect your domain to other things!
 
-This makes use of things only in Java SE 11, mostly [HttpServer](https://docs.oracle.com/en/java/javase/11/docs/api/jdk.httpserver/com/sun/net/httpserver/HttpServer.html).
+This makes use of things only in Java SE, mostly [HttpServer](https://docs.oracle.com/en/java/javase/11/docs/api/jdk.httpserver/com/sun/net/httpserver/HttpServer.html).
+
+You can specify different ports for different paths.
+
+**Note:** standard rules apply, you must have elevated permissions to run things on system ports (Like the first 1024 ports).  
 
 ## Usage
 
-`java -jar webredirect.jar`
+`java -jar webredirect.jar configFile`
 
 ### Config File
 
-The config file, `web-redirect.txt` is in this format.
+The config file specifies any number of redirects.
+
+Here's an example config file.
+
+```
+/google/
+80
+https://google.ca
+/bing/
+80
+https://bing.ca
+/google/
+81
+https://google.ca
+/businesscat
+80
+https://www.youtube.com/watch?v=qNhycX0XCJ0
+/redirect/
+80
+https://github.com/Raieen/WebRedirect
+```
+
+The general format of a redirect:
+
 ```
 path (ie. /)
 port (ie. 80)
--- src/io/github/raieen/webredirect/WebRedirect.java
+++ src/io/github/raieen/webredirect/WebRedirect.java
@@ -4,45 +4,98 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Web Redirection Server
+ * Simple web server using HttpServer.
+ * Give it some urls and it will redirect someone, somewhere!
+ */
 public class WebRedirect {
-    // Just redirecting one thing, so don't overcomplicate this.
-    private static final String CONFIG_FILE = "web-redirect.txt";
 
     // Permanent Redirect (See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
-    private final static int rCode = 308;
+    private final static int PERMANENT_REDIRECT = 308;
 
-    public static void main(String[] args) throws IOException {
-        File file = new File(CONFIG_FILE);
-        if (!file.exists()) {
-            System.out.println(String.format("%s does not exist. Creating it with default values.", CONFIG_FILE));
-            file.createNewFile(); // Shouldn't be false, because !file.exists()
-
-            FileWriter fileWriter = new FileWriter(CONFIG_FILE);
-            // Default Values
-            fileWriter.write("/\n");
-            fileWriter.write("80\n");
-            fileWriter.write("https://github.com/Raieen\n");
-            fileWriter.close();
+    public static void main(String[] args) {
+        // Arg check
+        if (args.length == 0) {
+            System.err.println("Usage: java -jar webredirect.jar config.txt");
+            return;
         }
 
+        String configFile = args[0];
+
+        File file = new File(configFile);
         try {
+            if (!file.exists()) {
+                System.err.println(String.format("%s does not exist, creating it with default values.", configFile));
+                if (!file.createNewFile()) {
+                    System.err.println(String.format("Error writing to %s", configFile));
+                    return;
+                }
+
+                // Default Values
+                FileWriter fileWriter = new FileWriter(configFile);
+                fileWriter.write(String.format("%s\n%d\n%s\n", "/test", (short) 8080, "https://github.com/Raieen"));
+                fileWriter.write(String.format("%s\n%d\n%s\n", "/test2", (short) 8080, "https://google.ca"));
+                fileWriter.close();
+            }
+            if (!file.canRead()) {
+                System.err.println(String.format("Cannot read file %s.", configFile));
+                return;
+            }
+
+            // Map [PORT, Server]
+            Map<Short, HttpServer> httpServerMap = new HashMap<>();
+
+            // Read config file and create a context for each server with redirection
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String path = bufferedReader.readLine();
-            int port = Integer.parseInt(bufferedReader.readLine());
-            String redirect = bufferedReader.readLine();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Path
+                String path = line;
 
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-            httpServer.createContext(path, exchange -> {
-                exchange.getResponseHeaders().set("Location", redirect);
-                exchange.sendResponseHeaders(rCode, -1);
-            });
+                // Port
+                short port;
+                try {
+                    port = Short.parseShort(bufferedReader.readLine());
+                    if (port < 0) throw new NumberFormatException();
+                } catch (NumberFormatException | IOException e) {
+                    // Port MAX_VALUE is USHORT MAX_VALUE. In practice, 0 (and others) are used/reserved
+                    System.err.println(String.format("Invalid port number. Port must be a number in [0, %d]", Short.MAX_VALUE * 2 + 1));
+                    return;
+                }
 
-            httpServer.start();
-            System.out.println(String.format("Started web redirect from localhost:%d%s to %s", port, path, redirect));
-        } catch (NumberFormatException e) {
-            // Port MAX_VALUE is USHORT MAX_VALUE. In practice, 0 (and handful) are used/reserved
-            System.out.println("Port is not a number, must be in [0, " + (Short.MAX_VALUE * 2 + 1) + "]");
+                // Redirect
+                String redirect = bufferedReader.readLine();
+                if (redirect == null) {
+                    System.err.println("Missing a redirection URL!");
+                    return;
+                }
+
+                HttpServer server;
+                if (httpServerMap.containsKey(port)) {
+                    server = httpServerMap.get(port);
+                } else {
+                    server = HttpServer.create(new InetSocketAddress(port), 0); // Use system default
+                    httpServerMap.put(port, server);
+                }
+
+                // Redirect context
+                server.createContext(path, exchange -> {
+                    exchange.getResponseHeaders().set("Location", redirect);
+                    exchange.sendResponseHeaders(PERMANENT_REDIRECT, -1);
+                });
+
+                System.out.println(String.format("Read web redirect from file, http://localhost:%d%s to %s", port, path, redirect));
+            }
+            for (HttpServer server : httpServerMap.values()) {
+                server.start();
+                System.out.println(String.format("Started server at http://localhost:%d", server.getAddress().getPort()));
+            }
+        } catch (IOException e) {
+            System.err.println(String.format("Something went wrong: %s", e.getMessage()));
         }
     }
 }
